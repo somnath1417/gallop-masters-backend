e0d4096a248c06166e5a49a997dfd778a5fc3ae7
somnath1417 <patilsomnath1417@gmail.com>
2026-04-24 22:17:26 +0530

    Fix CORS issue for Vercel

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const { frontendUrl } = require("./config/env");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173", "http://localhost:3000"],
+    origin: [frontendUrl, "http://localhost:5173", "http://localhost:3000"],
     credentials: true,
   }),
 );
